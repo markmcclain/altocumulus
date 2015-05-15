5b997ad41aafa14038cc5e78f0fb1d7b8b6597bf
Mark McClain <mark@mcclain.xyz>
2015-05-15 07:54:15 -0400

    Update for  hierarchical port binding

--- altocumulus/ml2/config.py
+++ altocumulus/ml2/config.py
@@ -1,8 +1,12 @@
 from oslo.config import cfg
 
 CUMULUS_DRIVER_OPTS = [
-    cfg.StrOpt('url',
-               help='Base URL for the Altocumulus API'),
+    cfg.StrOpt('scheme',
+               default='http',
+               help='Scheme for base URL for the Altocumulus API'),
+    cfg.IntOpt('protocol_port',
+               default='8140',
+               help='Protocol port for base URL for the Altocumulus API'),
 ]
 
 cfg.CONF.register_opts(CUMULUS_DRIVER_OPTS, 'ml2_cumulus')
--- altocumulus/ml2/driver.py
+++ altocumulus/ml2/driver.py
@@ -1,17 +1,23 @@
 import json
 
 from oslo.config import cfg
+from oslo_log import log as logging
 import requests
 from requests.exceptions import HTTPError
 
 from neutron.extensions import portbindings
+from neutron.i18n import _LE, _LI, _LW
 from neutron.plugins.ml2.common.exceptions import MechanismDriverError
 from neutron.plugins.ml2.driver_api import MechanismDriver
 
 from altocumulus.ml2 import config
 
-NETWORKS_URL = '{base}/networks/{network}'
-HOSTS_URL = '{base}/networks/{network}/hosts/{host}'
+LOG = logging.getLogger(__name__)
+NETWORKS_URL = '{scheme}://{base}:{port}/networks/{network}'
+HOSTS_URL = '{scheme}://{base}:{port}/networks/{network}/hosts/{host}'
+VXLAN_URL = '{scheme}://{base}:{port}/networks/{network}/vxlan/{vni}'
+LINUXBRIDGE_AGENT = 'Linux bridge agent'
+
 
 class CumulusMechanismDriver(MechanismDriver):
     """
@@ -21,57 +27,161 @@ class CumulusMechanismDriver(MechanismDriver):
     Inspired by the Arista ML2 mechanism driver
     """
     def initialize(self):
-        self.url = cfg.CONF.ml2_cumulus.url
+        self.scheme = cfg.CONF.ml2_cumulus.scheme
+        self.protocol_port = cfg.CONF.ml2_cumulus.protocol_port
 
-    def create_network_postcommit(self, context):
-        network_id = context.current['id']
-        vlan_id = context.network_segments[0].segmentation_id
+    def bind_port(self, context):
+        if context.binding_levels:
+            return  # we've already got a top binding
 
-        request = {
-            'vlan': vlan_id,
-        }
+        # assign a dynamic vlan
+        next_segment = context.allocate_dynamic_segment(
+            {'id': context.network.current, 'network_type': 'vlan'}
+        )
 
-        r = requests.put(
-            NETWORKS_URL.format(base=self.url, network=network_id),
-            data=json.dumps(request))
 
-        if r.status_code != requests.codes.ok:
-            raise MechanismDriverError()
+        context.continue_binding(
+            context.segments_to_bind[0]['id'],
+            [next_segment]
+        )
 
     def delete_network_postcommit(self, context):
         network_id = context.current['id']
+        vni = context.current['provider:segmentation_id']
 
-        r = requests.delete(
-            NETWORKS_URL.format(base=self.url, network=network_id))
+        agents = context._plugin.get_agents(
+            context._plugin_context,
+            filters={'agent_type': [LINUXBRIDGE_AGENT]}
+        )
 
-        if r.status_code != requests.codes.ok:
-            raise MechanismDriverError()
+
+        # remove vxlan from all hosts - a little unpleasant
+        for agent in agents:
+            try:
+                switch_mgmt_ip = agent['configurations']['switch_mgmt_ip']
+                actions = [
+                    VXLAN_URL.format(
+                        scheme=self.scheme,
+                        base=switch_mgmt_ip,
+                        port=self.protocol_port,
+                        vni=vni
+                    ),
+                    NETWORKS_URL.format(
+                        scheme=self.scheme,
+                        base=switch_mgmt_ip,
+                        port=self.protocol_port,
+                        network=network_id
+                    )
+                ]
+
+                for action in actions:
+                    r = requests.delete(action)
+
+                    if r.status_code != requests.codes.ok:
+                        LOG.info(
+                            _LI('Error during %s delete. HTTP Error:%s'),
+                            action, r.status_code
+                        )
+
+            except Exception, e:
+                # errors might be normal, but should improve this
+                LOG.info(_LI('Error during net delete. Error %s'), e)
 
     def create_port_postcommit(self, context):
+        if context.segments_to_bind:
+            self._add_to_switch(context)
+
+    def update_port_postcommit(self, context):
+        if context.host != context.original_host:
+            self._remove_from_switch(context.original)
+        self._add_to_switch(context)
+
+    def delete_port_postcommit(self, context):
+        self._remove_from_switch(context)
+
+    def _add_to_switch(self, context):
         port = context.current
 
         device_id = port['device_id']
         device_owner = port['device_owner']
         host = port[portbindings.HOST_ID]
         network_id = port['network_id']
+        vni = context.top_bound_segment['segmentation_id']
+        vlan = context.bottom_bound_segment['segmentation_id']
 
         if not (host and device_id and device_owner):
             return
 
+        agent = context.host_agents(LINUXBRIDGE_AGENT)
+
+        if not agent:
+            raise MechanismDriverError()
+
+        switch_mgmt_ip = agent[0]['configurations']['switch_mgmt_ip']
+
         r = requests.put(
-            HOSTS_URL.format(base=self.url, network=network_id, host=host))
+            NETWORKS_URL.format(
+                scheme=self.scheme,
+                base=switch_mgmt_ip,
+                port=self.protocol_port,
+                network=network_id
+            ),
+            data=json.dumps({'vlan': vlan})
+        )
 
         if r.status_code != requests.codes.ok:
             raise MechanismDriverError()
 
-    def delete_port_postcommit(self, context):
-        port = context.current
+        actions = [
+            HOSTS_URL.format(
+                scheme=self.scheme,
+                base=switch_mgmt_ip,
+                port=self.protocol_port,
+                network=network_id,
+                host=host
+            ),
+            VXLAN_URL.format(
+                scheme=self.scheme,
+                base=switch_mgmt_ip,
+                port=self.protocol_port,
+                network=network_id,
+                vni=vni
+            )
+        ]
 
+        for action in actions:
+            r = requests.put(action)
+
+            if r.status_code != requests.codes.ok:
+                raise MechanismDriverError()
+
+    def _remove_from_switch(self, context):
+        port = context.current
         host = port[portbindings.HOST_ID]
         network_id = port['network_id']
 
+        agent = context.host_agents(LINUXBRIDGE_AGENT)
+
+        if not agent:
+            raise MechanismDriverError()
+
+
+        switch_mgmt_ip = agent[0]['configurations']['switch_mgmt_ip']
+
         r = requests.delete(
-            HOSTS_URL.format(base=self.url, network=network_id, host=host))
+            HOSTS_URL.format(
+                scheme=self.scheme,
+                base=switch_mgmt_ip,
+                port=self.protocol_port,
+                network=network_id,
+                host=host
+            )
+        )
 
         if r.status_code != requests.codes.ok:
-            raise MechanismDriverError()
+            LOG.info(
+                _LI('error (%d) deleting port for %s on switch: %s'),
+                r.status_code,
+                host,
+                switch_mgmt_ip
+            )
