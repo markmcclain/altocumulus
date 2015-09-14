f7eaf2d80b2f70ac6f0d5794b30f9fd628122e34
Mark McClain <mark@mcclain.xyz>
2015-09-14 19:35:49 -0400

    Add support for devstack installation

---
+++ devstack/plugin.sh
@@ -0,0 +1,29 @@
+CUMULUS_DIR=${CUMULUS_DIR:-$DEST/altocumulus}
+
+if is_service_enabled neutron; then
+    if [[ "$1" == "source" ]]; then
+        # no-op
+        :
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        echo_summary "Installing Cumulus ML2"
+        setup_develop $CUMULUS_DIR
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # no-op
+        :
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # no-op
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # no-op
+        :
+    fi
+fi
